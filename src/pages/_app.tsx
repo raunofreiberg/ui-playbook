66e3c81494f68ec7a89c47e768aff3d141658801
raunofreiberg <freiberggg@gmail.com>
2020-07-09 14:03:54 +0300

    feat(tooltip): add example

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -3,51 +3,54 @@ import { DefaultSeo } from 'next-seo';
 import Link from 'next/link';
 import Head from 'next/head';
 import { MDXProvider } from '@mdx-js/react';
+import { RadixProvider, Tooltip, Button } from '@modulz/radix';
 import { Icon } from '../components';
 import './base.scss';
 
 export default function App({ Component, pageProps }) {
 	return (
-		<MDXProvider components={{ Icon }}>
-			<Head>
-				<meta name="keywords" content="ui playbook" />
-				<link rel="shortcut icon" href="/static/favicon.ico" />
-				<link rel="apple-touch-icon" sizes="180x180" href="/static/apple-touch-icon.png" />
-				<meta name="msapplication-TileColor" content="#6469FF" />
-				<meta name="theme-color" content="#6469FF" />
-			</Head>
-			<DefaultSeo
-				title="UI Playbook"
-				description="Recipes for common UI components"
-				openGraph={{
-					type: 'website',
-					url: 'https://uiplaybook.dev',
-					title: 'UI Playbook',
-					description: 'UI Playbook is a documented collection of common UI components',
-					images: [
-						{
-							url: '/static/playbook.png',
-							alt: 'UI Playbook',
-						},
-					],
-				}}
-			/>
-			<header>
-				<Link href="/">
-					<Icon icon="logo" />
-				</Link>
-				<a
-					href="https://github.com/raunofreiberg/ui-playbook"
-					target="_blank"
-					rel="noopener noreferrer"
-					aria-label="Open Github"
-				>
-					<Icon icon="github" />
-				</a>
-			</header>
-			<main>
-				<Component {...pageProps} />
-			</main>
-		</MDXProvider>
+		<RadixProvider>
+			<MDXProvider components={{ Icon, Tooltip, Button }}>
+				<Head>
+					<meta name="keywords" content="ui playbook" />
+					<link rel="shortcut icon" href="/static/favicon.ico" />
+					<link rel="apple-touch-icon" sizes="180x180" href="/static/apple-touch-icon.png" />
+					<meta name="msapplication-TileColor" content="#6469FF" />
+					<meta name="theme-color" content="#6469FF" />
+				</Head>
+				<DefaultSeo
+					title="UI Playbook"
+					description="Recipes for common UI components"
+					openGraph={{
+						type: 'website',
+						url: 'https://uiplaybook.dev',
+						title: 'UI Playbook',
+						description: 'UI Playbook is a documented collection of common UI components',
+						images: [
+							{
+								url: '/static/playbook.png',
+								alt: 'UI Playbook',
+							},
+						],
+					}}
+				/>
+				<header>
+					<Link href="/">
+						<Icon icon="logo" />
+					</Link>
+					<a
+						href="https://github.com/raunofreiberg/ui-playbook"
+						target="_blank"
+						rel="noopener noreferrer"
+						aria-label="Open Github"
+					>
+						<Icon icon="github" />
+					</a>
+				</header>
+				<main>
+					<Component {...pageProps} />
+				</main>
+			</MDXProvider>
+		</RadixProvider>
 	);
 }
