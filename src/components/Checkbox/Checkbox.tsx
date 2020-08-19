e5ff6273e53a4ae8bf359addd40d5930ea1605e2
raunofreiberg <freiberggg@gmail.com>
2020-08-19 18:08:58 +0300

    feat: add fieldset examples

--- src/components/Checkbox/Checkbox.tsx
+++ src/components/Checkbox/Checkbox.tsx
@@ -28,48 +28,51 @@ export default function Checkbox({ className, children, indeterminate, checked, 
 	);
 }
 
-export function Checkboxes() {
-	const [checkedItems, setCheckedItems] = React.useState([true, false]);
+export function Checkboxes(props: React.HTMLAttributes<HTMLFieldSetElement>) {
+	const [checkedItems, setCheckedItems] = React.useState([true, false, false]);
 	const firstCheckboxId = useId();
 	const secondCheckboxId = useId();
+	const thirdCheckboxId = useId();
 
 	const allChecked = checkedItems.every(Boolean);
 	const indeterminate = checkedItems.some(Boolean) && !allChecked;
 
 	return (
-		<div>
+		<fieldset {...props}>
+			<legend>
+				Choose ingredients <span aria-hidden>👇</span>
+			</legend>
 			<Checkbox
-				aria-controls={`${firstCheckboxId} ${secondCheckboxId}`}
+				aria-controls={`${firstCheckboxId} ${secondCheckboxId} ${thirdCheckboxId}`}
 				checked={allChecked}
 				indeterminate={indeterminate}
-				onChange={(e) => setCheckedItems([e.target.checked, e.target.checked])}
+				onChange={(e) => setCheckedItems([e.target.checked, e.target.checked, e.target.checked])}
 			>
-				Parent checkbox
+				All ingredients
 			</Checkbox>
-			<div
-				style={{
-					display: 'flex',
-					gap: 8,
-					flexDirection: 'column',
-					paddingLeft: '16px',
-					marginTop: 8,
-				}}
-			>
+			<div className={styles.checkboxes}>
 				<Checkbox
 					id={firstCheckboxId}
 					checked={checkedItems[0]}
-					onChange={(e) => setCheckedItems([e.target.checked, checkedItems[1]])}
+					onChange={(e) => setCheckedItems([e.target.checked, checkedItems[1], checkedItems[2]])}
 				>
-					Child checkbox 1
+					Tomato
 				</Checkbox>
 				<Checkbox
 					id={secondCheckboxId}
 					checked={checkedItems[1]}
-					onChange={(e) => setCheckedItems([checkedItems[0], e.target.checked])}
+					onChange={(e) => setCheckedItems([checkedItems[0], e.target.checked, checkedItems[2]])}
 				>
-					Child checkbox 2
+					Lettuce
+				</Checkbox>
+				<Checkbox
+					id={thirdCheckboxId}
+					checked={checkedItems[2]}
+					onChange={(e) => setCheckedItems([checkedItems[0], checkedItems[1], e.target.checked])}
+				>
+					Cucumber
 				</Checkbox>
 			</div>
-		</div>
+		</fieldset>
 	);
 }
